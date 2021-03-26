6ab5c1b4ef61e4871b77da40ec7a895d40ddff0f
Silas dos Santos Caimi <silas.caimi@cnpq.br>
2021-03-25 22:26:17 -0300

    adiciona arquivo de queries de criação do banco de dados

---
+++ queries.sql
@@ -0,0 +1,11 @@
+  mysql -u root -p
+  use fj21;
+  
+  create table tarefas (
+    id BIGINT NOT NULL AUTO_INCREMENT,
+    descricao VARCHAR(255),
+    finalizado BOOLEAN,
+    dataFinalizacao DATE,
+    primary key (id)
+  );
+
