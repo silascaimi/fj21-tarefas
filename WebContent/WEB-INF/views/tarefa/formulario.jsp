9632b62b05c089ca26c313ba8034b2fc6770fde3
Silas dos Santos Caimi <silas.caimi@cnpq.br>
2021-03-29 19:42:48 -0300

    validar-descricao

--- WebContent/WEB-INF/views/tarefa/formulario.jsp
+++ WebContent/WEB-INF/views/tarefa/formulario.jsp
@@ -1,5 +1,6 @@
 <%@ page language="java" contentType="text/html; charset=ISO-8859-1"
   pageEncoding="ISO-8859-1"%>
+<%@ taglib uri="http://www.springframework.org/tags/form" prefix="form"%>
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,6 +9,7 @@
 </head>
 <body>
   <h3>Adicionar tarefas</h3>
+  <form:errors path="tarefa.descricao" />
   <form action="adicionaTarefa" method="post">
     Descrição: <br />
     <textarea name="descricao" rows="5" cols="100"></textarea>
--- src/br/com/caelum/tarefas/controller/TarefasController.java
+++ src/br/com/caelum/tarefas/controller/TarefasController.java
@@ -1,6 +1,9 @@
 package br.com.caelum.tarefas.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import br.com.caelum.tarefas.dao.JdbcTarefaDao;
@@ -15,7 +18,11 @@ public class TarefasController {
 	}
 	
 	@RequestMapping("adicionaTarefa")
-	public String adiciona(Tarefa tarefa) {
+	public String adiciona(@Valid Tarefa tarefa, BindingResult result) {
+		
+		if(result.hasFieldErrors("descricao")) {
+			return "tarefa/formulario";
+		}
 		
 		JdbcTarefaDao dao = new JdbcTarefaDao();
 		dao.adiciona(tarefa);
